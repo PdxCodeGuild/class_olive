628d64745ee7e37d5229207d2e5c9dbf8be671fb
robbieo15 <robbieorocks@gmail.com>
2022-08-16 20:38:49 -0400

    lab2b in progress

--- code/robert/python/lab2b.py
+++ code/robert/python/lab2b.py
@@ -20,6 +20,7 @@ teens_to_text = {
     17 : 'Seventeen',
     18 : 'Eighteen',
     19 : 'Nineteen',
+    10 : 'Ten'
 }
 tens_to_text = {
     2 : 'Twenty',
@@ -32,13 +33,32 @@ tens_to_text = {
     9 : 'Ninety'
 }
 
+hundreds_to_text = {
+    10 : 'One Hundred and',
+    20 : 'Two Hundred and',
+    30 : 'Three Hundred and',
+    40 : 'Four Hundred and',
+    50 : 'Five Hundred and',
+    60 : 'Six Hundred and',
+    70 : 'Seven Hundred and',
+    80 : 'Eight Hundred and',
+    90 : 'Nine Hundred and'
+}
 
-x = 25
+x = 115
+hundreds_digit = x%1000
 tens_digit = x//10
 ones_digit = x%10
 
+print(hundreds_digit)
 print(tens_digit)
 print(ones_digit)
 
 if x > 19:
-   print(f' {tens_to_text[tens_digit]} - {ones_to_text[ones_digit]}')
+    print(f' {tens_to_text[tens_digit]}-{ones_to_text[ones_digit]}')
+elif x < 20 and x > 9:
+    print(f'{teens_to_text[x]}')
+elif x < 10 and x >= 0:
+    print(ones_to_text[ones_digit])
+elif x > 99 and x < 1000:
+    print()
