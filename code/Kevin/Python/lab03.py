562f51f0f0a09a35d17a39a8c3dd9deffe1848ad
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2022-08-16 20:47:17 -0400

    Start of lab03

---
+++ code/Kevin/Python/lab03.py
@@ -0,0 +1,17 @@
+'''
+This lab converts numbers characters to their english representation.
+'''
+
+# user_input = input('''Please input a number between 1 and 10 to convert to english.
+# >: ''')
+
+x = 67
+tens_digit = x//10
+ones_digit = x%10
+
+print(tens_digit, ones_digit)
+
+if tens_digit == 6
+    tens_digit = 'sixty'
+
+if 
