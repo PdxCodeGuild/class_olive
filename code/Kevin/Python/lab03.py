24fe9bdf5f5f7d331f7d48c6fc20c9fe97e6378e
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2022-08-17 19:29:49 -0400

    version with comments

--- code/Kevin/Python/lab03.py
+++ code/Kevin/Python/lab03.py
@@ -2,15 +2,7 @@
 This lab converts numbers characters to their english representation.
 '''
 
-# user_input = input('''Please input a number between 1 and 10 to convert to english.
-# >: ''')
-
-# x = 67
-# tens_digit = x//10
-# ones_digit = x%10
-
-# print(tens_digit, ones_digit)
-
+#Dictionary for the ones place or hundreds
 Convert_ones_place_dict = {
     0: '',
     1: 'one',
@@ -24,6 +16,7 @@ Convert_ones_place_dict = {
     9: 'nine'
 }
 
+#Dictionary for tens place
 Convert_tens_place_dict = {
     0: '',
     2: 'twenty',
@@ -36,9 +29,12 @@ Convert_tens_place_dict = {
     9: 'ninety'
 }
 
-list_of_outliers = [11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90]
+#List of outliers for conditional statements
+list_of_outliers = [10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90]
 
+#Dictionary for outliers
 Convert_outliers = {
+    10:'ten',
     11:'eleven',
     12:'twelve',
     13:'thirteen',
@@ -58,28 +54,28 @@ Convert_outliers = {
     90:'ninty',
 }
 
-
+#While loop for breaking out when printing and future possible utility
 while True:
-    User_input = int(input('Please enter a number 0-99.\n>: '))
-    if User_input == 0:
+    User_input = int(input('Please enter a number 0-999.\n>: '))     #Ask user for a number and convert to an "int". No error correction.
+    if User_input == 0:     #If user input is 0, just print 0.
         print('zero')
         break
-    elif User_input in list_of_outliers:
+    elif User_input in list_of_outliers:        #If user input is in the list of outliers, just print the outliers
         print(Convert_outliers[User_input])
         break
-    elif User_input > 99:
-        hundreds_digit = User_input//100
-        tens_digit = (User_input - hundreds_digit*100)
-        if tens_digit in list_of_outliers:
-            print(f'{Convert_ones_place_dict[hundreds_digit]} hundred {Convert_outliers[tens_digit]}')
+    elif User_input > 99:       #If user input is between 100 and 999, handle those numbers
+        hundreds_digit = User_input//100        #Get the hundreds digit by doing floor division with user input
+        subtracted_input = (User_input - hundreds_digit*100)      #Get rid of the 100th place so we can check to see if it's an outlier.
+        if subtracted_input in list_of_outliers:
+            print(f'{Convert_ones_place_dict[hundreds_digit]} hundred {Convert_outliers[subtracted_input]}')      #If the english words for the remander of "subtracted_input are an outlier, print "'x' hundred 'outlier'""
             break
-        tens_digit = (User_input - hundreds_digit*100)//10
-        ones_digit = User_input%10
-        print(f'{Convert_ones_place_dict[hundreds_digit]} hundred {Convert_tens_place_dict[tens_digit]} {Convert_ones_place_dict[ones_digit]}' )
+        tens_digit = subtracted_input//10       #If "subtracted_input" is not an outlier, do floor division it for the 10th place.
+        ones_digit = User_input%10              #Then, do module division on the user's input. The result will be the ones place.
+        print(f'{Convert_ones_place_dict[hundreds_digit]} hundred {Convert_tens_place_dict[tens_digit]} {Convert_ones_place_dict[ones_digit]}' )        #Print ones places dict ' hundred' tens place dict ' ' ones palce dict
         break
-    tens_digit = User_input//10
+    tens_digit = User_input//10     #If user input is between 0 and 99, floor division the tens place and modulas the ones place
     ones_digit = User_input%10
-    print(f'{Convert_tens_place_dict[tens_digit]}{Convert_ones_place_dict[ones_digit]}')
+    print(f'{Convert_tens_place_dict[tens_digit]} {Convert_ones_place_dict[ones_digit]}')       #Print the ones place and the tens place.
     break
 
        
