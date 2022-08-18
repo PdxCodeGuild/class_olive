0d69393aacf008fdf3519c9b6661a17f67cbddd9
matthew chimenti <matthewosu@gmail.com>
2022-08-18 14:42:52 -0700

    adding wheel of terror

---
+++ code/matt/wheel_of_terror/questions.py
@@ -0,0 +1,42 @@
+terms = [
+    {
+        "topic": "How to get a value from a dictionary",
+        "definition": "dictionary name then [] with the key for the pair"
+    },
+    {
+        "topic": "How to get a value from a list",
+        "definition": "list name then [] with an integer"
+    },
+    {
+        "topic": "Conditional Statement",
+        "definition": "if statements"
+    },
+    {
+        "topic": "whats wrong with this: 'hello'.upper",
+        "definition": "method does not have its call"
+    },
+    {
+        "topic": "API",
+        "definition": "API is the acronym for Application Programming Interface, which is a software intermediary that allows two applications to talk to each other. Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you’re using an API.."
+    },
+    {
+        "topic": "index",
+        "definition": "how position of the element in a sequence"
+    },
+    {
+        "topic": "What is a variable",
+        "definition": "a named storage space"
+    },
+    {
+        "topic": "What is the datatype of a variable",
+        "definition": "whatever is stored in it"
+    },
+    {
+        "topic": "How many index positions does this number have 123456 ",
+        "definition": "None. Integers do not have index positions"
+    }, 
+    {
+        "topic": "When would you use a for loop",
+        "definition": "to go over every item in a sequence. example string or list"
+    },
+]
