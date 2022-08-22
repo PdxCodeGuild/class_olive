6c279429320cfb27856611776b06fc3c8068cb8f
tooblekane <tooblekane@gmail.com>
2022-08-22 15:03:23 -0700

    Finally got it, ty for the help

--- code/tim/python/lab08.py
+++ code/tim/python/lab08.py
@@ -1,5 +1,4 @@
 # Lab 08 Pick6
-from asyncio import current_task
 import random
 
 # 1 Generate a list of 6 random numbers representing the winning ticket
@@ -11,76 +10,74 @@ import random
 #   7 Add to your balance the winnings from your matches
 # 8 After the loop, print the final balance (Hint: This will be negative)
 
-# def pick6():
-#     winner = []
-#     balance = 0   
-#         for index in range(6):     
-#         winner.append(random.randint(1,99))
-      
-#     print(f"The winning ticket is {winner}")
-      
-#     for index in range(10):
-#         ticket = []
-#         balance -= 2
-#         # print(f"Your balance is {balance}")       # Works - Your balance is -2   Your balance is -4 ....
-#         for index in range(6):
-#             ticket.append(random.randint(1,99))
-#     # return winner    
-
-# def num_matches(winner, ticket):
-#     if winner[0] != ticket[0]:
-#         print('no match on 0')
-
-# pick6()    
-
-################################
-
 balance = 0
-
+# matches = 0
 
 def ticket():
     numbers = []
     for x in range(6):
         numbers.append(random.randint(0,99))
-    return numbers                                      # Why does this display None every loop????  If this is where it's coming from
+    # print(numbers)
+    return numbers
 
 def compare_matches(winner, ticket):
     matches = 0
     if winner[0] == ticket[0]:
-        # print(f"Winning ticket had {winner[0]} and you had {ticket[0]}.")    # Just making sure this works, will build out
         matches += 1
     
-    elif winner[1] == ticket[1]:
-        matches += 1
-    
-    elif winner[2] == ticket[2]:
-        matches += 1
-    
-    elif winner[3] == ticket[3]:
+    if winner[1] == ticket[1]:
         matches += 1
 
-    elif winner[4] == ticket[5]:
+    if winner[2] == ticket[2]:
+        matches += 1
+
+    if winner[3] == ticket[3]:
+        matches += 1
+
+    if winner[4] == ticket[4]:
         matches += 1    
-    
-    elif winner[5] == ticket[5]:
+
+    if winner[5] == ticket[5]:
         matches += 1
 
-    if matches > 0:
-        print(f"You had {matches} matches.")
-    else:
-        print(f"You had no matches.")
+    return matches
 
 
 winning_ticket = ticket()
+# winning_ticket = [16, 59, 49, 83, 3, 4]
+
+
 print(f"The winning ticket is {winning_ticket}")
 
-for x in range(3):
+winnings = 0
+for x in range(100000):
     balance -= 2
     current_ticket = ticket()
+    # current_ticket = [16, 59, 49, 83, 3, 4]    # You had 1 matches  -- even when all should be matches
     # print(f"Ticket is {current_ticket}")
-    print(compare_matches(winning_ticket, current_ticket))    
+    matches = compare_matches(winning_ticket,current_ticket)
 
 
+    if matches == 1:
+        winnings += 4
+    
+    elif matches == 2:
+        winnings += 7
 
-# print(f"Your balance is {balance}.")
-# print(compare_matches(winning_ticket, current_ticket))    
+    elif matches == 3:
+        winnings += 100
+
+    elif matches == 4:
+        winnings += 50000
+
+    elif matches == 5:
+        winnings += 1000000
+    
+    elif matches == 6:
+        winnings += 25000000
+    
+print(f"Your cost is {balance}.")
+print(f"You won {winnings}.")
+print(winnings + balance)
+
+print(f"Your ROI is {(winnings + balance) / balance}") 
