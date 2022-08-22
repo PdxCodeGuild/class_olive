352f1074e528e579004bef1cc6d0658a7fe8e1df
Nathan Huckabay <nathanhuckabay@gmail.com>
2022-08-22 18:21:35 -0500

    Fixed pick 6 lab

--- code/nathan/python/lab5.py
+++ code/nathan/python/lab5.py
@@ -6,19 +6,28 @@ def pick_6():
         numbers.append(random.randint(1, 99))
     return numbers
 
+def test_ticket(winning, hopeful):
+    winnings = {0 : 0, 1 : 4, 2 : 7, 3 : 100, 4 : 50_000, 5 : 1_000_000, 6 : 25_000_000}
+    wins = 0
+    counter = 0
+    while counter < len(winning):
+        if winning[counter] == hopeful[counter]:
+            wins += 1
+        counter += 1
+    return winnings[wins] - 2
+    
+        
+
 winning_ticket = pick_6()
 
-count = 0
+total_winnings = 0
+expenses = 0
 
 for x in range(100_000):
-    if winning_ticket == pick_6():
-        count += 1
+    total_winnings += test_ticket(winning_ticket, pick_6())
+    expenses += 2
 
-balance = -2.0
-
-winnings = {0 : 0, 1 : 4, 2 : 7, 3 : 100, 4 : 50_000, 5 : 1_000_000, 6 : 25_000_000}
-
-if count > 6:
-    print("That's crazy.")
+if total_winnings > 0:
+    print(f" You won {total_winnings} dollars! Your ROI is {(total_winnings - expenses) / expenses}")
 else:
-    print(f"You won ${balance + winnings[count]}! Your ROI is ${(balance - 2)/2}")
+    print(f"Sorry, you didn't win. You ended with {total_winnings} dollars Your ROI is {(total_winnings - expenses) / expenses}")
