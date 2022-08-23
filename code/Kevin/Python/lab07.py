5b6a8d21aafc72bfa4b291fe0d3170e54d646790
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2022-08-23 18:47:23 -0400

    version 2

--- code/Kevin/Python/lab07.py
+++ code/Kevin/Python/lab07.py
@@ -2,7 +2,7 @@
 This program encodes strings into ROT13
 '''
 
-
+#Build a dict for letters for referencing keys.
 letter_dict = {
     ' ':0,
     'a':1,
@@ -33,20 +33,27 @@ letter_dict = {
     'z':26
 }
 
+#Build a list with letters for referencing the index.
 letter_list = [' ','a','b','c','d','e','f','g','h','i','j','k',
 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-user_input_str = 'abcdefghijklmnop'
+#Ask the user for a string to decode, then ask their desired ROT.
+user_input_str = input('Please enter a string for decoding.\n>: ')
+user_input_rot = int(input('Now, enter how much ROT.\n>: '))
+
+#Declare a variable for storing the output string.
 output_str = ''
 
-
+#Loop through the user input, making x each character.
 for x in user_input_str:
-    letter_value = 0
-    if x != ' ':
-        letter_value = letter_dict[x] + 13
-        if letter_value > 26:
-            letter_value -= 26
-    output_str += letter_list[letter_value]
+    letter_value = 0        #Declare and reset a variable for storing the value associated with "x"s character... 
+                            # at the appropriate key in "letter_dict."
+    if x != ' ':        #If "x" is a space character, go straight to line 55.
+        letter_value = letter_dict[x] + user_input_rot      #Add to "letter_value" the value at "x"s key in "letter_dict", 
+                                                            # plus the user defined ROT.
+        while letter_value > 26:        #For as long as "letter_value" is over 26,
+            letter_value -= 26          # reduce it by 26.
+    output_str += letter_list[letter_value]     #Concatenate "output_str" with the character at the index of "letter_list."
 
 print(output_str)
     
