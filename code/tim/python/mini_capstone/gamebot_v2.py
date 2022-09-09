da8e39b80859f14278bb3035c10c973a26d297c3
tooblekane <tooblekane@gmail.com>
2022-09-08 17:24:08 -0700

    functional, working on discord bot portion now

--- code/tim/python/mini_capstone/gamebot_v2.py
+++ code/tim/python/mini_capstone/gamebot_v2.py
@@ -1,35 +1,48 @@
 import requests, json
+from pprint import pprint
 
 selected_games = []
 
 # Lists top Steam sales
 def top_deals():
-    counter = 1
-    page = 1
-    sale_url = "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=5&pageSize=10&storeID=1"
-    payload={}
-    headers = {}
-    response = requests.request("GET", sale_url, headers=headers, data=payload)
-    data = json.loads(response.text)
+    page = 0
+    while True:
+        counter = 1
+        payload={}
+        headers = {}
+        deals_url = f"https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=5&pageSize=10&pageNumber={page}"
+        deals_response = requests.request("GET", deals_url, headers=headers, data=payload)
+        deals_data = json.loads(deals_response.text)
+        print("Current Best Deals on Steam:\n")
+
+        for game in deals_data:
+            print(f"{counter}. {game['title']}\nSale price {game['salePrice']} - Normal price {game['normalPrice']}")
+            counter += 1
+        
+        print(f"\nPage {page+1} of {deals_response.headers['x-total-page-count']}")
+        selection = input("Which game do you want to see more information on? Or use + and - to change pages: ")
+        if selection == '+':
+            page += 1
+        elif page == 0 and selection == '-':
+            page == 0
+        elif selection == '-':
+            page -= 1
+        else:         
+            break
     
-    print("Current Best Deals on Steam:\n")
-    for game in data:
-        print(f"{counter}. {game['title']}\nSale price {game['salePrice']} - Normal price {game['normalPrice']}")
-        counter += 1
-    selection = int(input("Which game do you want to see more information on? Or enter Next Page: ")) - 1
-    selected_game = data[selection]
+    selected_game = deals_data[int(selection)-1]
     view_game(selected_game)
 
 # View selected game, does 2 get requests because they can't have all the useful info in just one now can they?    
 def view_game(selected_game):
     selected_games.append(selected_game)
-    gameid_url = f"https://www.cheapshark.com/api/1.0/games?id={selected_game['gameID']}" 
-    dealid_url = f"https://www.cheapshark.com/api/1.0/deals?id={selected_game['dealID']}" 
     payload={}
     headers = {}
+    gameid_url = f"https://www.cheapshark.com/api/1.0/games?id={selected_game['gameID']}" 
+    dealid_url = f"https://www.cheapshark.com/api/1.0/deals?id={selected_game['dealID']}" 
     gameid_response = requests.request("GET", gameid_url, headers=headers, data=payload)
-    gameid_data = json.loads(gameid_response.text)
     dealid_response = requests.request("GET", dealid_url, headers=headers, data=payload)
+    gameid_data = json.loads(gameid_response.text)
     dealid_data = json.loads(dealid_response.text)
     
     # Checks if the current price is the cheapest it has ever been, adds a note if so
@@ -42,29 +55,47 @@ def view_game(selected_game):
     print(f"""\n{gameid_data['info']['title']} - Metacritic Score - {dealid_data['gameInfo']['metacriticScore']}%          
     Sale price ${dealid_data['gameInfo']['salePrice']} - Normal price ${dealid_data['gameInfo']['retailPrice']} - Savings {gameid_data['deals'][0]['savings']}% 
     Cheapest Price Ever - ${cheapest_ever}
-            
+
     Steam Rating - {dealid_data['gameInfo']['steamRatingPercent']}% {dealid_data['gameInfo']['steamRatingText']} with {dealid_data['gameInfo']['steamRatingCount']} reviews
     Link to sale - https://www.cheapshark.com/redirect?dealID={gameid_data['deals'][0]['dealID']}
     """)
 
 # Search current Steam sales
 def search_deals():
-    counter = 1
+    page = 0
     game_search = input("Enter a game to search for: ")
-    # search_url = f"https://www.cheapshark.com/api/1.0/games?title={game_search}&limit=10&exact=0" # Think this is full game search / not just sales - Add later?
-    search_url = f"https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=5&pageSize=10&storeID=1&title={game_search}"
-    payload={}
-    headers = {}
-    response = requests.request("GET", search_url, headers=headers, data=payload)
-    data = json.loads(response.text)
-    
-    print("Results sorted by Deal Rating:\n")
-    for game in data:
-        print(f"{counter}. {game['title']}\nSale price {game['salePrice']} - Normal price {game['normalPrice']}")
-        counter += 1
-    selection = int(input("Which game do you want to see more information on? Or enter Next Page: ")) - 1
-    selected_game = data[selection]
+    while True:
+        counter = 1
+        payload={}
+        headers = {}
+        search_url = f"https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=5&pageSize=10&title={game_search}&pageNumber={page}"
+        search_response = requests.request("GET", search_url, headers=headers, data=payload)
+        search_data = json.loads(search_response.text)
+        print("Results sorted by Deal Rating:\n")
+        
+        for game in search_data:
+            print(f"{counter}. {game['title']}\nSale price {game['salePrice']} - Normal price {game['normalPrice']}")
+            counter += 1
+        
+        print(f"\nPage {page+1} of {search_response.headers['x-total-page-count']}")
+        search_selection = input("Which game do you want to see more information on? Or use + and - to change pages: ")
+        if search_selection == '+':
+            page += 1
+        elif page == 0 and search_selection == '-':
+            page == 0
+        elif search_selection == '-':
+            page -= 1
+        else:         
+            break
+        
+    selected_game = search_data[int(search_selection)-1]
     view_game(selected_game)
+
+def set_alert():
+    print("Not yet")
+
+def view_alerts():
+    print("Not yet")
 
 def gamebot():
     
@@ -82,11 +113,113 @@ def gamebot():
         if choice == '2':
             search_deals()
         if choice == '3':
-            pass
+            set_alert()
         if choice == '4':
-            pass
+            view_alerts()
         if choice == '5':
             exit()
             
 
 gamebot()
+
+'''
+deals_data  
+[  
+{'dealID': 'dJNCeHkZV3iaXZQFBSpYh3B2tz6ZuMvBaFpI6d1QYiU%3D',
+  'dealRating': '10.0',
+  'gameID': '154838',
+  'internalName': 'TUMBLESTONE',
+  'isOnSale': '1',
+  'lastChange': 1662409231,
+  'metacriticLink': '/game/pc/tumblestone',
+  'metacriticScore': '91',
+  'normalPrice': '24.99',
+  'releaseDate': 1468281600,
+  'salePrice': '2.49',
+  'savings': '90.036014',
+  'steamAppID': '269710',
+  'steamRatingCount': '126',
+  'steamRatingPercent': '88',
+  'steamRatingText': 'Very Positive',
+  'storeID': '1',
+  'thumb': 'https://cdn.cloudflare.steamstatic.com/steam/apps/269710/capsule_sm_120.jpg?t=1646164113',
+  'title': 'Tumblestone'},
+]
+
+gameid_data 
+{  
+ 'cheapestPriceEver': {'date': 1640201740, 'price': '2.49'},
+ 'deals': [{'dealID': 'dJNCeHkZV3iaXZQFBSpYh3B2tz6ZuMvBaFpI6d1QYiU%3D',
+            'price': '2.49',
+            'retailPrice': '24.99',
+            'savings': '90.036014',
+            'storeID': '1'},
+           {'dealID': 'nDJpJR66SXSgVdWyeQYWrEEXcOiQKDjx4fn%2BiXtfbXk%3D',
+            'price': '24.99',
+            'retailPrice': '24.99',
+            'savings': '0.000000',
+            'storeID': '21'},
+           {'dealID': 'vBcBR5HWelc%2BiepW0ceo1R9oJ%2B9wpjDbDU2s07JCbvk%3D',
+            'price': '24.99',
+            'retailPrice': '24.99',
+            'savings': '0.000000',
+            'storeID': '11'},
+           {'dealID': 'VRCmFsaIviXjxxM0Ns0s1R8VWAkA7%2FUVACGrMZje5jU%3D',
+            'price': '24.99',
+            'retailPrice': '24.99',
+            'savings': '0.000000',
+            'storeID': '3'},
+           {'dealID': 'uf1WXGkGc9YQpyJrlpvtyzxH0CaPqNhY%2FdWyFzqmUAw%3D',
+            'price': '24.99',
+            'retailPrice': '24.99',
+            'savings': '0.000000',
+            'storeID': '30'}],
+ 'info': {'steamAppID': '269710',
+          'thumb': 'https://cdn.cloudflare.steamstatic.com/steam/apps/269710/capsule_sm_120.jpg?t=1646164113',
+          'title': 'Tumblestone'}}
+
+
+dealid_data
+{
+ 'cheaperStores': [],
+ 'cheapestPrice': {'date': 0},
+ 'gameInfo': {'gameID': '154838',
+              'metacriticLink': '/game/pc/tumblestone',
+              'metacriticScore': '91',
+              'name': 'Tumblestone',
+              'publisher': 'N/A',
+              'releaseDate': 1468281600,
+              'retailPrice': '24.99',
+              'salePrice': '2.49',
+              'steamAppID': '269710',
+              'steamRatingCount': '126',
+              'steamRatingPercent': '88',
+              'steamRatingText': 'Very Positive',
+              'steamworks': '1',
+              'storeID': '1',
+              'thumb': 'https://cdn.cloudflare.steamstatic.com/steam/apps/269710/capsule_sm_120.jpg?t=1646164113'}}
+
+search_data
+[
+{
+  'dealID': 'dJNCeHkZV3iaXZQFBSpYh3B2tz6ZuMvBaFpI6d1QYiU%3D',
+  'dealRating': '10.0',
+  'gameID': '154838',
+  'internalName': 'TUMBLESTONE',
+  'isOnSale': '1',
+  'lastChange': 1662409231,
+  'metacriticLink': '/game/pc/tumblestone',
+  'metacriticScore': '91',
+  'normalPrice': '24.99',
+  'releaseDate': 1468281600,
+  'salePrice': '2.49',
+  'savings': '90.036014',
+  'steamAppID': '269710',
+  'steamRatingCount': '126',
+  'steamRatingPercent': '88',
+  'steamRatingText': 'Very Positive',
+  'storeID': '1',
+  'thumb': 'https://cdn.cloudflare.steamstatic.com/steam/apps/269710/capsule_sm_120.jpg?t=1646164113',
+  'title': 'Tumblestone'}
+]
+'''
