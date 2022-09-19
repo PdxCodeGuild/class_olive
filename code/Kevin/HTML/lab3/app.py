079ac7e8e43ccc71a36302a80b21fe6301689be0
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2022-09-19 18:04:36 -0400

    progress capture

--- code/Kevin/HTML/lab3/app.py
+++ code/Kevin/HTML/lab3/app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# blog_post = {
+#     'title': 
+# }
+
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('index_about.html')
