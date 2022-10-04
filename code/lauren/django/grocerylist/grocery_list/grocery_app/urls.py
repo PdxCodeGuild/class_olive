2080f8daacaf15e9d979823f3a8d7fc09e8cbf3f
laurenornelas325 <lauren32596@gmail.com>
2022-10-04 17:51:56 -0500

    lauren-django-grocerylist

--- code/lauren/django/grocerylist/grocery_list/grocery_app/urls.py
+++ code/lauren/django/grocerylist/grocery_list/grocery_app/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
         path('', views.home_view, name='home'),
     path('add_new_item', views.new_item, name='add_new_item'),
     path('toggle/<int:id>/', views.toggle_in_cart, name='toggle_in_cart'),
-     path('delete/<int:id>/', views.delete_item, name='delete_route')
     
 ]
