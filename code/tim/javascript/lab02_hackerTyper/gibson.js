97b11eccc503edd2d0d4f9e069ab02dabaccb8fc
tooblekane <tooblekane@gmail.com>
2022-11-01 14:04:57 -0700

    little more before class

--- code/tim/javascript/lab02_hackerTyper/gibson.js
+++ code/tim/javascript/lab02_hackerTyper/gibson.js
@@ -2,14 +2,14 @@ let slice = ''
 let pressedKey = ''
 let currentIndex = 16
 let terminalOutput = document.querySelector('#terminal_output')
-let message1 = document.querySelector('#message1')
-// let firewall1 = document.querySelector('#firewall1')
-// let firewall2 = document.querySelector('#firewall2')
-let firewall = document.querySelector('#warning_backtrace')
-let firewallMsg = 0
-terminalOutput.innerHTML = hacker_text.slice(0,15)
+let warningCount = 0
+let warningMsg = document.querySelector('#warning_msg')
+let warningBtn = document.querySelector('#warning_btn')
+let warningDiv = document.querySelector('#warnings')
+let warnings = ['Detected intrusion in power cable!','System has been breached!','First firewall breached!','Second firewall breached!','Third firewall breached!','Fourth firewall breached!','Final firewall breached!']
 
-// document.querySelector("terminalOutput").scrollTop = document.querySelector("terminalOutput").scrollHeight
+terminalOutput.innerHTML = hacker_text.slice(0,15)
+// console.log(hacker_text.length) //6165
 
 let getRandomInt = function(min, max) {
     min = Math.ceil(min);
@@ -17,30 +17,14 @@ let getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-// let firewallBreach = function(index){
-//     if (index >= 20 && index <= 39 && firewallMsg === 0){
-//         console.log('firewall 1 - ' + index)
-//         firewall.innerHTML += '<ul>firewall 1</ul>'
-//         ++firewallMsg
-//     } 
-//     if (index >= 40 && index <= 59 && firewallMsg === 1){
-//         console.log('firewall 2 - ' + index)
-//         firewall.innerHTML += '<ul>firewall 2</ul>'
-//         ++firewallMsg
-// }}
-
-var myVar = setInterval(function() {
-    myTimer();
-}, 1000);
-    
-function myTimer() {
-    var d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+let clock = setInterval(function() {trayClock();}, 1000);
+function trayClock() {
+    let newTime = new Date();
+    document.getElementById("clock").innerHTML = newTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 }
 
-
 document.addEventListener('keypress', (event) => {
-    let randomInt = getRandomInt(10,29)
+    let randomInt = getRandomInt(10,20)
     // console.log(`start: ${currentIndex}`)
     // console.log(`random: ${randomInt}`)
     updatedIndex = currentIndex + randomInt
@@ -49,9 +33,25 @@ document.addEventListener('keypress', (event) => {
     currentIndex = updatedIndex
     // console.log(`end: ${currentIndex}`)
     terminalOutput.innerHTML = `<div id="terminalOutputText">${hacker_text.slice(0,15)} ${slice}</div>`
-    // firewallBreach(currentIndex)
-    // if (currentIndex >= 55 && currentIndex <= 75 ){
-    //     console.log('!!!!!true - ' + currentIndex)
-    //     message1.innerHTML = `<ul>ruh roh i see you</ul>`
-    // }
+    warningPopups()
 })
+
+let warningPopups = function() {
+    let startmin = 880
+    let startmax = 910
+    if (currentIndex > startmin && currentIndex < startmax){
+        console.log(currentIndex, startmin, startmax, warningCount)
+        // console.log('!!!!! ' + warnings[warningCount])
+        warningDiv.style.display = 'block'
+        warningMsg.innerHTML = warnings[warningCount]
+        warningCount++
+        startmin += 880 
+        startmax += 880
+        console.log(currentIndex, startmin, startmax, warningCount)
+    }
+}
+
+warningBtn.onclick = function() {
+    warningDiv.style.display = 'none'
+}
+
