c622890e2af2255592c50bbfbac1e985ed0c07a1
tooblekane <tooblekane@gmail.com>
2022-11-15 18:00:55 -0800

    think i'm about done, just some tinkering and cleaning left

--- code/tim/javascript/lab05_quiz_game/script.js
+++ code/tim/javascript/lab05_quiz_game/script.js
@@ -1,100 +1,108 @@
 const app = Vue.createApp({
     data(){
         return{
-            questionsList: '',
-            amount: 3,
-            currentQuestion: 1,
+            quizList: [],
+            quizAmount: 10,
+            quizCategory: null,
+            currentQuestion: {},
+            currentQuestionIndex: -1,
+            originalAnswers: [],
+            randomAnswers: [],
+            totalScore: 0,
+            gameStatus: 'waiting',
+            resultImage: { backgroundImage: "url()"},
+            playerResult: '',
         }
     },
     methods: {
-        getQuiz(){
-            
+        setCategory(buttonCategory){
+            this.quizCategory = buttonCategory
+            this.startQuiz()
+        },
+        
+        startQuiz(){
             axios({
                 method: 'get',
                 url: 'https://opentdb.com/api.php',
                 params: {
-                    amount: this.amount,
-                    category: 11,
+                    amount: this.quizAmount,
+                    category: this.quizCategory,
                     difficulty: 'easy',
                     type: 'multiple',
                 }
             }).then((response) => {
-                console.log(response.data.results)
-                this.questionsList = response.data.results
-                // console.log(this.questionsList[0].category)
-                
-                for (let i = 0; i < this.questionsList.length; i++){
-                    // console.log(`${this.questionsList}`)
-                    // console.log(this.difficulty)
-                    // console.log('hi')
-                    if (this.currentQuestion < this.amount)
-                    {
-                        this.responseQ = this.questionsList[i].question
-                        this.correctAnswer = this.questionsList[i].correct_answer
-                        console.log(this.amount)
-                        break
-                        // return
-                    }
-                }
+                this.quizList = response.data.results
+                this.gameStatus = 'playing'
+                this.nextQuestion()
+
             })
-        }
+        },
+
+        decode(str) {
+            let txt = document.createElement("textarea");
+            txt.innerHTML = str;
+            return txt.value;
+        },
+
+        nextQuestion(){
+            if (this.currentQuestionIndex === (this.quizAmount -1)){
+                this.gameOver()
+            }
+
+            else if (this.currentQuestionIndex <= (this.quizAmount -1)){
+                this.resultImage['backgroundImage'] = ""
+                this.currentQuestionIndex++
+                this.currentQuestion = this.quizList[this.currentQuestionIndex]
+                this.originalAnswers = this.currentQuestion.incorrect_answers
+                this.originalAnswers.push(this.currentQuestion.correct_answer)
+                this.randomAnswers = this.originalAnswers.sort((a, b) => 0.5 - Math.random());
+                console.log('Answer - ' + this.currentQuestion.correct_answer)
+            }
+        },
+
+        checkAnswer(buttonValue){
+            if (buttonValue === this.currentQuestion.correct_answer){
+                this.resultImage['backgroundImage'] = "url(./correct.png)"
+                this.totalScore += (this.currentQuestionIndex + 1) * 100
+            }
+            else{
+                this.resultImage['backgroundImage'] = "url(./incorrect.png)"
+                console.log('Wrong.  Answer is ' + this.currentQuestion.correct_answer)
+                this.totalScore -= (this.currentQuestionIndex + 1) * 100
+            }
+            setTimeout(() => {this.nextQuestion(); }, 1000);
+        },
+
+        gameOver(){
+            this.gameStatus = 'over'
+            console.log('game over')
+            
+            if (this.totalScore > 0){
+                console.log(this.totalScore + 'is positive')
+                this.playerResult = 'winner'
+            }
+            else{
+                console.log(this.totalScore + 'is negative')
+                this.playerResult = 'loser'
+            }
+        },
+
+        resetQuiz(){
+            this.quizList = []
+            this.quizAmount = 10
+            this.quizCategory = null
+            this.currentQuestion = {}
+            this.currentQuestionIndex = -1
+            this.originalAnswers = []
+            this.randomAnswers = []
+            this.totalScore = 0
+            this.resultImage = { backgroundImage: "url()"}
+            this.gameStatus = 'waiting'
+            this.playerResult = ''
+            location.reload()
+        },
     },
     mounted(){
-        this.getQuiz()
 
-    },
-    setup(){
-        // axios({
-        //     method: 'get',
-        //     url: 'https://opentdb.com/api.php',
-        //     params: {
-        //         amount: 10,
-        //         category: 11,
-        //         difficulty: 'easy',
-        //         type: 'multiple',
-        //     }
-        // }).then((response) => {
-        //     this.questionsList = response.data.results
-        //     console.log(this.questionsList)
-            
-        //     for (let i = 0; i < this.questionsList.length; i++){
-        //         // console.log(`${this.questionsList}`)
-        //         // console.log(this.difficulty)
-        //         // console.log('hi')
-        //         this.responseQ = this.questionsList[i].question
-        //         // this.correctA
-                
-        //     }
-        //     return
-        // })
     }
 })
-
-
-// function getData(){
-//     axios({
-//         method: 'get',
-//         url: 'https://opentdb.com/api.php',
-//         params: {
-//             amount: 10,
-//             category: 11,
-//             difficulty: 'easy',
-//             type: 'multiple',
-//         }
-//     }).then((response) => {
-//         let data = response.data.results[0]
-//         console.log(response.data.results[0])
-//         // console.table(response.data.results[0])
-//         // testData = data.difficulty
-
-//         question.innerHTML = data.question
-//         answer1.innerHTML = data.incorrect_answers[0]
-//         answer2.innerHTML = data.incorrect_answers[1]
-//         answer3.innerHTML = data.incorrect_answers[2]
-//         answer4.innerHTML = data.correct_answer
-
-//         return response
-//     })
-// }
-
-// getData()
